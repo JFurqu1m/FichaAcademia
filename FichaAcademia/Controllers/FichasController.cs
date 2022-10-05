58e4b4313abb5503eb2d9ccb22c757b84865107f
Jeferson Furquim <jeeh.tsg@gmail.com>
2022-10-05 15:26:32 -0300

    CRUD Fichas (Part_One)

---
+++ FichaAcademia/Controllers/FichasController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FichaAcademia.AcessoDados;
+using FichaAcademia.Dominio.Models;
+using FichaAcademia.AcessoDados.Interfaces;
+
+namespace FichaAcademia.Controllers
+{
+    public class FichasController : Controller
+    {
+        private readonly IAlunoRepositorio _alunoRepositorio;
+        private readonly IFichaRepositorio _fichaRepositorio;
+
+        public FichasController(IAlunoRepositorio alunoRepositorio, IFichaRepositorio fichaRepositorio)
+        {                      
+            _alunoRepositorio = alunoRepositorio;
+            _fichaRepositorio = fichaRepositorio;
+        }
+
+       
+        public async Task<IActionResult> Index(int AlunoId)
+        {
+            ViewBag.AlunoId = AlunoId;
+            return View(await _fichaRepositorio.PegarTodasFichasAlunoId(AlunoId));
+        }
+
+        public async Task<IActionResult> Details(int FichaId)
+        {
+            var ficha = await _fichaRepositorio.PegarFichaPeloId(FichaId);
+                
+            if (ficha == null)
+            {
+                return NotFound();
+            }
+
+            return View(ficha);
+        }
+
+       
+        public IActionResult Create(int AlunoId)
+        {
+            Ficha ficha = new Ficha();
+            ficha.AlunoId = AlunoId;
+            return View(ficha);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FichaId,Nome,Cadastro,Validade,AlunoId")] Ficha ficha)
+        {
+            ficha.Cadastro = DateTime.Parse(DateTime.Now.ToShortDateString());
+            ficha.Validade = DateTime.Parse(DateTime.Now.AddYears(1).ToShortDateString());
+
+
+            if (ModelState.IsValid)
+            {
+                await _fichaRepositorio.Inserir(ficha);
+                return RedirectToAction(nameof(Index), new { AlunoId = ficha.AlunoId });
+            }           
+            return View(ficha);
+        }
+
+       
+        public async Task<IActionResult> Edit(int FichaId)
+        {
+            var ficha = await _fichaRepositorio.PegarFichaPeloId(FichaId);
+            if (ficha == null)
+            {
+                return NotFound();
+            }
+            
+            return View(ficha);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int FichaId, [Bind("FichaId,Nome,Cadastro,Validade,AlunoId")] Ficha ficha)
+        {
+            if (FichaId != ficha.FichaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _fichaRepositorio.Atualizar(ficha);
+                return RedirectToAction(nameof(Index), new {AlunoId = ficha.AlunoId });
+            }
+           
+            return View(ficha);
+        }
+      
+        [HttpPost]
+        public async Task<JsonResult> Delete(int FichaId)
+        {
+            await _fichaRepositorio.Excluir(FichaId);
+            return Json("Ficha excluída com sucesso.");
+        }
+
+        public async Task<JsonResult> FichaExiste(string Nome, int FichaId)
+        {
+            if(FichaId == 0)
+            {
+                if (await _fichaRepositorio.FichaExiste(Nome))
+                    Json("Ficha já cadastrada");
+
+                return Json(true);
+            } else
+            {
+                if (await _fichaRepositorio.FichaExiste(Nome, FichaId))
+                    Json("Ficha já cadastrada");
+
+                return Json(true);
+            }
+        }
+       
+    }
+}
